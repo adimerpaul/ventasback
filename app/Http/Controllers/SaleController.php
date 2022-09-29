2d53b8a738c3477518a0a7b007d59eaef7528906
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-09-29 11:16:10 -0400

    contrl v

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -403,7 +403,7 @@ class SaleController extends Controller
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-        $result = $conn->query("SELECT * from cliente where codigo='$codigo'");
+        $result = $conn->query("SELECT * from cliente where codigo='$codigo' and estado='ACTIVO'");
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
